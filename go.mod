15970873484a534c2c61b5862a78fc33507bb404
Adphi <philippe.adrien.nousse@gmail.com>
2025-05-02 17:36:46 +0200

    init
    Signed-off-by: Adphi <philippe.adrien.nousse@gmail.com>

---
+++ go.mod
@@ -0,0 +1,152 @@
+module go.linka.cloud/flux-helm-release-alerts
+
+go 1.24.0
+
+toolchain go1.24.2
+
+godebug default=go1.23
+
+require (
+	github.com/blang/semver/v4 v4.0.0
+	github.com/fluxcd/helm-controller/api v1.2.0
+	github.com/fluxcd/source-controller/api v1.5.0
+	github.com/go-openapi/strfmt v0.23.0
+	github.com/onsi/ginkgo/v2 v2.23.4
+	github.com/onsi/gomega v1.37.0
+	github.com/prometheus/alertmanager v0.28.1
+	github.com/spf13/pflag v1.0.6
+	go.linka.cloud/grpc-toolkit v0.4.4-0.20250414073415-533a0ea43a28
+	helm.sh/helm/v4 v4.0.0-20250425235407-31e22b9866af
+	k8s.io/apimachinery v0.33.0
+	k8s.io/client-go v0.33.0
+	sigs.k8s.io/controller-runtime v0.20.4
+)
+
+require (
+	cel.dev/expr v0.19.1 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20250102033503-faa5f7b0171c // indirect
+	github.com/MakeNowJust/heredoc v1.0.0 // indirect
+	github.com/Masterminds/semver/v3 v3.3.1 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bombsimon/logrusr/v4 v4.0.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/chai2010/gettext-go v1.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/emicklei/go-restful/v3 v3.12.1 // indirect
+	github.com/evanphx/json-patch v5.9.11+incompatible // indirect
+	github.com/evanphx/json-patch/v5 v5.9.11 // indirect
+	github.com/exponent-io/jsonpath v0.0.0-20210407135951-1de76d718b3f // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fluxcd/cli-utils v0.36.0-flux.13 // indirect
+	github.com/fluxcd/pkg/apis/acl v0.6.0 // indirect
+	github.com/fluxcd/pkg/apis/kustomize v1.9.0 // indirect
+	github.com/fluxcd/pkg/apis/meta v1.10.0 // indirect
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/go-errors/errors v1.5.1 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-logr/zapr v1.3.0 // indirect
+	github.com/go-openapi/analysis v0.23.0 // indirect
+	github.com/go-openapi/errors v0.22.0 // indirect
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/jsonreference v0.21.0 // indirect
+	github.com/go-openapi/loads v0.22.0 // indirect
+	github.com/go-openapi/runtime v0.28.0 // indirect
+	github.com/go-openapi/spec v0.21.0 // indirect
+	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/go-openapi/validate v0.24.0 // indirect
+	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/btree v1.1.3 // indirect
+	github.com/google/cel-go v0.23.2 // indirect
+	github.com/google/gnostic-models v0.6.9 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
+	github.com/google/pprof v0.0.0-20250403155104-27863c87afa6 // indirect
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.4-0.20250319132907-e064f32e3674 // indirect
+	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.24.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/liggitt/tabwriter v0.0.0-20181228230101-89fcab3d43de // indirect
+	github.com/mailru/easyjson v0.9.0 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/moby/spdystream v0.5.0 // indirect
+	github.com/moby/term v0.5.2 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/monochromegane/go-gitignore v0.0.0-20200626010858-205db1a8cc00 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/mxk/go-flowrate v0.0.0-20140419014527-cca7078d478f // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.22.0 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.62.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spf13/cobra v1.9.1 // indirect
+	github.com/stoewer/go-strcase v1.3.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	github.com/xlab/treeprint v1.2.0 // indirect
+	go.mongodb.org/mongo-driver v1.14.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.58.0 // indirect
+	go.opentelemetry.io/otel v1.34.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.33.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.33.0 // indirect
+	go.opentelemetry.io/otel/metric v1.34.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.33.0 // indirect
+	go.opentelemetry.io/otel/trace v1.34.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.4.0 // indirect
+	go.uber.org/automaxprocs v1.6.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
+	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/oauth2 v0.29.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/term v0.31.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	golang.org/x/time v0.11.0 // indirect
+	golang.org/x/tools v0.32.0 // indirect
+	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20241209162323-e6fa225c2576 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241209162323-e6fa225c2576 // indirect
+	google.golang.org/grpc v1.68.1 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
+	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/api v0.33.0 // indirect
+	k8s.io/apiextensions-apiserver v0.33.0 // indirect
+	k8s.io/apiserver v0.33.0 // indirect
+	k8s.io/cli-runtime v0.33.0 // indirect
+	k8s.io/component-base v0.33.0 // indirect
+	k8s.io/klog/v2 v2.130.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20250318190949-c8a335a9a2ff // indirect
+	k8s.io/kubectl v0.33.0 // indirect
+	k8s.io/utils v0.0.0-20250321185631-1f6e0b77f77e // indirect
+	oras.land/oras-go/v2 v2.5.0 // indirect
+	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.31.2 // indirect
+	sigs.k8s.io/json v0.0.0-20241014173422-cfa47c3a1cc8 // indirect
+	sigs.k8s.io/kustomize/api v0.19.0 // indirect
+	sigs.k8s.io/kustomize/kyaml v0.19.0 // indirect
+	sigs.k8s.io/randfill v1.0.0 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.6.0 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
+)
---
+++ internal/controller/helmrelease_controller.go
@@ -0,0 +1,269 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	url2 "net/url"
+	"os"
+	"time"
+
+	"github.com/blang/semver/v4"
+	helmv2 "github.com/fluxcd/helm-controller/api/v2"
+	sourcev1 "github.com/fluxcd/source-controller/api/v1"
+	"github.com/go-openapi/strfmt"
+	apiv2 "github.com/prometheus/alertmanager/api/v2/client"
+	"github.com/prometheus/alertmanager/api/v2/client/alert"
+	"github.com/prometheus/alertmanager/api/v2/models"
+	"helm.sh/helm/v4/pkg/repo"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+var requeueAfter = 5 * time.Minute
+
+const (
+	AlertName     = "HelmReleaseUpdateAvailable"
+	AlertSeverity = "warning"
+)
+
+// HelmReleaseReconciler reconciles a HelmRelease object
+type HelmReleaseReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+	API    *apiv2.AlertmanagerAPI
+}
+
+// +kubebuilder:rbac:groups=helm.toolkit.fluxcd.io,resources=helmreleases,verbs=get;list;watch
+// +kubebuilder:rbac:groups=helm.toolkit.fluxcd.io,resources=helmreleases/status,verbs=get
+// +kubebuilder:rbac:groups=source.toolkit.fluxcd.io,resources=helmrepositories,verbs=get;list;watch
+// +kubebuilder:rbac:groups=source.toolkit.fluxcd.io,resources=helmrepositories/status,verbs=get
+// +kubebuilder:rbac:groups=source.toolkit.fluxcd.io,resources=helmcharts,verbs=get;list;watch
+// +kubebuilder:rbac:groups=source.toolkit.fluxcd.io,resources=helmcharts/status,verbs=get
+
+func (r *HelmReleaseReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := logf.FromContext(ctx)
+	var filters = []string{
+		"alertname=" + AlertName,
+		"severity=" + AlertSeverity,
+		"apiVersion=helm.toolkit.fluxcd.io/v2",
+		"kind=HelmRelease",
+		"namespace=" + req.Namespace,
+		"name=" + req.Name,
+	}
+	log.V(6).Info("retrieving alerts")
+	alerts, err := r.API.Alert.GetAlerts(&alert.GetAlertsParams{Context: ctx, Filter: filters})
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	var hr helmv2.HelmRelease
+	log.V(6).Info("retrieving helm release")
+	if err := r.Get(ctx, req.NamespacedName, &hr); err != nil {
+		if !apierrors.IsNotFound(err) {
+			log.V(6).Info("helm release not found")
+			return ctrl.Result{}, err
+		}
+		if len(alerts.GetPayload()) == 0 {
+			log.V(6).Info("no alerts to delete")
+			return ctrl.Result{}, nil
+		}
+		return r.expireAlerts(ctx, alerts.GetPayload())
+	}
+	if hr.Spec.Chart == nil || hr.Spec.Chart.Spec.SourceRef.Kind != "HelmRepository" {
+		log.Info("no helm repository, skipping")
+		return ctrl.Result{}, nil
+	}
+	if hr.Status.LastAttemptedRevision == "" {
+		log.Info("no last attempted revision, skipping")
+		return ctrl.Result{}, nil
+	}
+	if hr.Spec.Chart.Spec.SourceRef.Namespace == "" {
+		hr.Spec.Chart.Spec.SourceRef.Namespace = req.Namespace
+	}
+	cver, err := semver.ParseTolerant(hr.Status.LastAttemptedRevision)
+	if err != nil {
+		log.Info("non semver version, skipping", "version", hr.Status.LastAttemptedRevision)
+		return ctrl.Result{}, nil
+	}
+	var hrepo sourcev1.HelmRepository
+	log.V(6).Info("retrieving helm repository")
+	if err := r.Get(ctx, types.NamespacedName{Name: hr.Spec.Chart.Spec.SourceRef.Name, Namespace: hr.Spec.Chart.Spec.SourceRef.Namespace}, &hrepo); err != nil {
+		if apierrors.IsNotFound(err) {
+			log.V(6).Info("helm repository not found")
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+	if hrepo.Status.URL == "" {
+		log.V(6).Info("helm repository not ready")
+		return ctrl.Result{}, nil
+	}
+	log.V(6).Info("retrieving helm repository index")
+	index, err := getIndex(ctx, hrepo.Status.Artifact.URL)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	c, ok := index.Entries[hr.Spec.Chart.Spec.Chart]
+	if !ok {
+		log.Info("chart not found in index")
+		return ctrl.Result{}, nil
+	}
+	last := cver
+	for _, v := range c {
+		ver, err := semver.ParseTolerant(v.Version)
+		if err != nil {
+			log.V(6).Info("non semver version, skipping", "version", v.Version)
+		}
+		if ver.GT(last) {
+			last = ver
+		}
+	}
+	if last.EQ(cver) {
+		log.Info("release up to date", "version", cver)
+		return r.expireAlerts(ctx, alerts.GetPayload())
+	}
+	log.Info("new version available", "current", cver.String(), "last", last.String())
+	if len(alerts.GetPayload()) == 0 {
+		log.V(6).Info("creating alert")
+		if _, err := r.API.Alert.PostAlerts(&alert.PostAlertsParams{Context: ctx, Alerts: models.PostableAlerts{createAlert(hr, cver, last)}}); err != nil {
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{}, nil
+	}
+	var pa models.PostableAlerts
+	for _, v := range alerts.GetPayload() {
+		if v.Labels["current"] == cver.String() && v.Labels["last"] == last.String() {
+			log.V(6).Info("alert up to date")
+			continue
+		}
+		a := models.PostableAlert{
+			StartsAt: strfmt.DateTime(time.Now()),
+			Annotations: map[string]string{
+				"summary": fmt.Sprintf("%s/%s %s: %s", hr.Namespace, hr.Name, fmt.Sprintf("New version available: %s", last), fmt.Sprintf("Current version: %s", cver)),
+			},
+			Alert: models.Alert{
+				Labels: models.LabelSet{
+					"alertname":  AlertName,
+					"severity":   AlertSeverity,
+					"apiVersion": hr.APIVersion,
+					"kind":       hr.Kind,
+					"namespace":  hr.Namespace,
+					"name":       hr.Name,
+					"current":    cver.String(),
+					"last":       last.String(),
+				},
+			},
+		}
+		o := a
+		o.Labels = v.Labels
+		o.EndsAt = strfmt.DateTime(time.Now())
+		pa = append(pa, &o, &a)
+	}
+	if len(pa) == 0 {
+		log.Info("alert already exists")
+		return ctrl.Result{}, nil
+	}
+	log.Info("updating alert", "current", cver.String(), "last", last.String())
+	if _, err := r.API.Alert.PostAlerts(&alert.PostAlertsParams{Context: ctx, Alerts: pa}); err != nil {
+		return ctrl.Result{}, err
+	}
+	return ctrl.Result{RequeueAfter: requeueAfter}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *HelmReleaseReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&helmv2.HelmRelease{}).
+		Named("helmreleases").
+		Complete(r)
+}
+
+func (r *HelmReleaseReconciler) expireAlerts(ctx context.Context, alerts models.GettableAlerts) (ctrl.Result, error) {
+	if len(alerts) == 0 {
+		return ctrl.Result{}, nil
+	}
+	var as models.PostableAlerts
+	for _, v := range alerts {
+		as = append(as, &models.PostableAlert{
+			StartsAt:    *v.StartsAt,
+			EndsAt:      strfmt.DateTime(time.Now()),
+			Annotations: v.Annotations,
+			Alert: models.Alert{
+				Labels: v.Labels,
+			},
+		})
+	}
+	if _, err := r.API.Alert.PostAlerts(&alert.PostAlertsParams{Context: ctx, Alerts: as}); err != nil {
+		return ctrl.Result{}, err
+	}
+	return ctrl.Result{}, nil
+}
+
+func getIndex(ctx context.Context, url string) (*repo.IndexFile, error) {
+	u, err := url2.Parse(url)
+	if err != nil {
+		return nil, err
+	}
+	if e := os.Getenv("SOURCE_ENDPOINT"); e != "" {
+		u.Host = e
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to get index: %s", res.Status)
+	}
+	var index repo.IndexFile
+	if err := json.NewDecoder(res.Body).Decode(&index); err != nil {
+		return nil, err
+	}
+	return &index, nil
+}
+
+func createAlert(hr helmv2.HelmRelease, current, last semver.Version) *models.PostableAlert {
+	return &models.PostableAlert{
+		StartsAt: strfmt.DateTime(time.Now()),
+		Annotations: map[string]string{
+			"summary": fmt.Sprintf("%s/%s %s (%s)", hr.Namespace, hr.Name, fmt.Sprintf("New version available: %s", last), fmt.Sprintf("current version: %s", current)),
+		},
+		Alert: models.Alert{
+			Labels: models.LabelSet{
+				"alertname":  AlertName,
+				"severity":   AlertSeverity,
+				"apiVersion": hr.APIVersion,
+				"kind":       hr.Kind,
+				"namespace":  hr.Namespace,
+				"name":       hr.Name,
+				"current":    current.String(),
+				"last":       last.String(),
+			},
+		},
+	}
+}
